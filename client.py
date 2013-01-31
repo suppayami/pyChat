e9d14a2c1d9b6d5e9e585c1dddeafc181addcced
SuppaYami <cuongnt.hn@gmail.com>
2013-01-31 11:08:51 +0700

    Remake server side, update client side.

--- client.py
+++ client.py
@@ -1,2 +1,36 @@
 import socket
-import threading
+import thread
+import os
+
+
+HOST = "localhost"
+PORT = 1902
+
+def read_data(sock):
+    rb = sock.makefile("rb", -1)
+    return rb.readline().strip().decode("utf-8")
+    
+def send_data(sock, data):
+    sock.sendall(data)
+    
+def fetchData():
+    while True:
+        data = read_data(sock)
+        if data:
+            backlog.append(data)
+            os.system(['clear','cls'][os.name == 'nt'])
+            for line in backlog:
+                print(line)
+        
+sock = socket.socket()
+backlog = []
+try:
+    sock.connect((HOST, PORT))
+    thread.start_new_thread(fetchData, ())   
+    while True:
+        string = raw_input("")
+        string = string + "\n"
+        send_data(sock, string)
+
+except:
+    sock.close()
--- server.py
+++ server.py
@@ -1,38 +1,9 @@
 import SocketServer
 import json
-import threading
 
 
-class ServerHandling(threading.Thread):
-    server = None
-    def run(self):
-        ServerHandling.server = ListenServer(("", 1902), ClientHandler)
-        ServerHandling.server.serve_forever()
-        
-class ClientHandling(threading.Thread):
-    def __init__(self, handler):
-        self.handler = handler
-        threading.Thread.__init__(self)
-        
-    def run(self):
-        while True:
-            data = self.handler.read_data()
-            if not data:
-                print("Disconnected: " + str(self.handler.client_address))
-                break
-            self.handler.send_data(data)
-            
-class CommandHandling(threading.Thread):
-    def run(self):
-        while True:
-            string = raw_input(" ")
-            if string.upper() == "CLOSE":
-                ServerHandling.server.shutdown()
-                ServerHandling.server.server_close()
-                break
-            else:
-                for client in ClientHandler.clients:
-                    client.request.sendall("From server: " + string + "\n")
+HOST = "localhost"
+PORT = 1902
 
 class ListenServer(SocketServer.ThreadingTCPServer):
     pass
@@ -42,24 +13,22 @@ class ClientHandler(SocketServer.StreamRequestHandler):
     def handle(self):
         ClientHandler.clients.append(self)
         print("Connected: " + str(self.client_address))
-
-        clientHandling = ClientHandling(self)
-        clientHandling.start()
-        commandHandling.join()
+        while True:
+            data = self.read_data()
+            if not data:
+                print("Disconnected: " + str(self.client_address))
+                break
+            else:
+                self.send_data(data)
     
     def read_data(self):
-        return self.rfile.readline().strip().decode("utf-8")
+        return self.rfile.readline().strip()
         
     def send_data(self, data):
-        data = data + "\n"
-        data = str(self.client_address) + ": " + data
+        data = str(self.client_address) + ": " + data + "\n"
         for client in ClientHandler.clients:
             client.wfile.write(data)
             client.wfile.flush()
         
-serverHandling = ServerHandling()
-serverHandling.start()
-
-commandHandling = CommandHandling()
-commandHandling.start()
-commandHandling.join()
+server = ListenServer((HOST, PORT), ClientHandler)
+server.serve_forever()
